0ad3e4cd51da82f328cb31799e8b9fef566ac779
sivafastbokz <sivaharshanfastbokz@gmail.com>
2023-09-29 23:34:16 +0530

    setup compeleted

--- src/component/Chat.jsx
+++ src/component/Chat.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function ChatApp(){
+    const [socket,setSocket]=useState(null)
+    const [message, setMessage] = useState('');
+
+    const ws = new WebSocket('ws://localhost:5000')
+    ws.onopen = ()=>{
+        console.log('websocket connected')
+        setSocket(ws)
+    }
+    const sendMessage = () => {
+        if (socket && message.trim() !== '') {
+          socket.send(message);
+          setMessage('');
+        }
+      };
     return(
         <>
-      <h3>testing app</h3>
+        <input type='text' onChange={(event)=>{setMessage(event.target.value)}}></input>
+        <button onClick={sendMessage}>send</button>
         </>
     )
 }
